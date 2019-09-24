92836f8c47d154cdce6fb80c31625695233c124e
Ben Levy <general@simplexwebsites.com>
2019-09-23 22:07:31 -0400

    The code works!

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ const express = require("express"),
 	app = express(),
 	request = require("request");
 
-app.get("/",(req,res)=>{
+app.get("/data/allUsers",(req,res)=>{
 	request("http://us.data.vatsim.net/vatsim-data.txt",(err,response,body)=>{
 		let data  = body.trim();
 		data = data.split(":");
 		data = data.filter(data=>data.length > 2);
-		res.send(data));
-		//Next step - Convert to JSON. Pfffft
+		console.log(Object.keys(data).length);
+		let aircraftData = [];
+		for(i=0;i<Object.keys(data).length;i++){
+			//Loop through array, delete random shit, and find point A to B (ex: Pilot's Name to Remarks), and create some JSON for that person.
+			//23 positions after "PILOT" is each pilot's data.
+			if(data[i]=="PILOT"){
+				while(i<Object.keys(data).length){
+					aircraftData.push(data[i]);
+					i++;
+				}
+
+			}
+		
+		}
+		res.send(JSON.stringify(aircraftData));
+		//Next step - Give each Pilot his/her own JSON "block".
 	});
 });
 
